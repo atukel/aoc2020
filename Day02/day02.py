d0a0664d81478528f095f9601c891c17aa1a75a1
nou <75327262+atukel@users.noreply.github.com>
2020-12-02 20:30:37 +0300

    Update day02.py

--- Day02/day02.py
+++ Day02/day02.py
@@ -16,9 +16,6 @@ def part2():
     for line in passwordList:
         characterCount, key, password = line.split(" ")
         firstPos, SecondPos = [int(i) for i in characterCount.split("-")]
-        check = False
         if (key[0] == password[firstPos - 1]) ^ (key[0] == password[SecondPos - 1]):
-            check = True
-        if check:
             passwordCount += 1
     print(passwordCount)
